87ab6b6cdc8c859c7e4d4dff3cd85ddc77a42d24
sjvb <86975801+sjvb@users.noreply.github.com>
2021-07-05 21:52:25 +0530

    Create 3_sumIntegers20.py

---
+++ 3_sumIntegers20.py
@@ -0,0 +1,10 @@
+i = int(input("Enter integer1: "))
+j = int(input("Enter integer2: "))
+
+sumInts = i + j
+
+# show sum as 20 if sum is between 15 to 20
+if sumInts >= 15 and sumInts <= 20:
+    sumInts = 20
+
+print("Sum of Integers = ", sumInts)
